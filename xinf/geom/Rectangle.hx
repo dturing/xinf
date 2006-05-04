597067193316df8c9bb2573aef7a5d14fae88efe
dturing <mail@danielturing.com>
2006-05-04 23:58:52 +0000

    add geometry classes (old Matrix, Point, new Rectangle).

---
+++ xinf/geom/Rectangle.hx
@@ -0,0 +1,28 @@
+package xinf.geom;
+
+class Rectangle {
+    public var x:Float;
+    public var y:Float;
+    public var w:Float;
+    public var h:Float;
+    
+    public function new( _x:Float, _y:Float, _w:Float, _h:Float ) {
+        x = _x;
+        y = _y;
+        w = _w;
+        h = _h;
+    }
+    
+    public function clone() : Point {
+        return new Rectangle( x, y, w, h );
+    }
+    
+    public function offset( dx:Float, dy:Float ) : Void {
+        x+=dx;
+        y+=dy;
+    }
+    
+    public function toString() : String {
+        return("("+x+","+y+"-"+w+"x"+h+")");
+    }
+}
