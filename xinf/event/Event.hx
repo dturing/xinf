441dd5355c8e0f12c3bf24d32874dfcb77e06855
dturing <mail@danielturing.com>
2006-05-05 13:22:22 +0000

    move event module to xinf.

---
+++ xinf/event/Event.hx
@@ -0,0 +1,11 @@
+package xinf.event;
+
+class Event {
+    public static var ENTER_FRAME:String = "enterFrame";
+    
+    public property type(default,null) : String;
+    
+    public function new( t:String ) {
+        type = t;
+    }
+}
