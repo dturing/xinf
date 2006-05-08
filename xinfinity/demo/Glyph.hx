c571503f821a09720a3f429b2fd4ebf220c6e97c
dturing <mail@danielturing.com>
2006-05-08 16:27:00 +0000

    forgot.

---
+++ xinfinity/demo/Glyph.hx
@@ -0,0 +1,76 @@
+package xinfinity.demo;
+
+import xinfinity.font.Font;
+import xinfinity.graphics.Object;
+
+class Glyph extends Object {
+    private static var _font:Font = new FontReader("/home/dan/.fonts/vera.ttf").getFont();
+    private var glyph:Int;
+
+    public function new() {
+        super();
+        glyph = 65;
+    }
+    
+    public function setGlyph( g:Int ) {
+        glyph = g;
+        changed();
+    }
+        
+    private function renderLine( x1:Float, y1:Float, x2:Float, y2:Float ) {
+        GL.Begin( GL.LINES );
+            GL.Vertex3f( x1, y1, 0 );
+            GL.Vertex3f( x2, y2, 0 );
+        GL.End();
+    }
+    
+    private function _renderGraphics() :Void {
+        var lineHeight:Float = (_font.ascender + _font.descender + _font.height)*50;
+
+        var g = _font.getGlyph( glyph );
+        var w = g.advance;
+        var b = 0.1;
+
+        GL.Translatef( -.5, -.5, 1.0 );
+        GL.Scalef( 124., 124., 1.0 );
+
+      // text
+        GL.PushMatrix();
+        GL.Color4f( 1., 1., 1., .5 );
+        g.render();
+        GL.PopMatrix();
+        
+      // metrics lines
+        GL.Color4f( 1., 1., 1., 1. );
+        GL.LineWidth( 1 );
+        
+        renderLine( -b, 0, g.advance+(2*b), 0 );
+        renderLine( -b, -_font.ascender, g.advance+(2*b), -_font.ascender );
+        renderLine( -b, -_font.descender, g.advance+(2*b), -_font.descender );
+
+        renderLine( -2*b, -_font.descender, -2*b, -_font.descender-_font.height );
+        
+        renderLine( 0, -b, 0, b );
+        renderLine( g.advance, -b, g.advance, b );
+                            
+
+    }
+    
+    private function _render() :Void {
+        _renderGraphics();
+        super._render();
+    }
+
+    private function _renderSimple() :Void {
+        var w = width;
+        var h = height;
+
+      //  background
+        GL.Begin( GL.QUADS );
+            GL.Vertex3f( w , 0., 0. );
+            GL.Vertex3f( w , h , 0. );
+            GL.Vertex3f( 0., h , 0. );
+            GL.Vertex3f( 0., 0., 0. );
+        GL.End();
+    }
+}
---
+++ xinfinity/font/Font.hx
@@ -0,0 +1,116 @@
+package xinfinity.font;
+
+import xinfinity.graphics.Polygon;
+import xinfinity.graphics.Contour;
+import xinf.geom.Point;
+
+class Glyph {
+    private var polygon:Polygon;
+    public var advance:Float;
+    private var list:Int;
+    
+    public function new( p:Polygon, adv:Float ) {
+        polygon = p;
+        advance = adv;
+        list = 0;
+    }
+    
+    public function render() {
+        polygon.render();
+        GL.Translatef( advance, .0, .0 );
+    }
+}
+
+class Font {
+    private var glyphs:Array<Glyph>;
+    
+    public property family_name(default,null):String;
+    public property style_name(default,null):String;
+    public property ascender(default,null):Float;
+    public property descender(default,null):Float;
+    public property height(default,null):Float;
+    public property underline_thickness(default,null):Float;
+    public property underline_position(default,null):Float;
+    public property units_per_EM(default,null):Float;
+    
+    public function new() {
+        glyphs = new Array<Glyph>();
+        untyped glyphs.__resize(0xff);
+    }
+    
+    public function getGlyph( character:Int ):Glyph {
+        var g:Glyph = glyphs[character];
+        return( g );
+    }
+}
+
+class FontReader {
+    private var font:Font;
+    private var glyph:Glyph;
+    private var contour:Contour;
+    private var polygon:Polygon;
+    private var scale:Float;
+
+    public function new( name:String ) {
+        font = new Font();
+        polygon = new Polygon();
+        
+        var _f = FT.LoadFont( untyped name.__s, untyped "abcdefghijklmnopqrstuvwxyz".__s, 1024<<6, 1024<<6 );
+        scale = 1./(1024<<6);
+//        trace("Font Height: "+_f.height );
+       
+        for( field in [ 
+            "family_name", "style_name", "file_name"
+            ] ) {
+            var h = untyped __dollar__hash(field.__s);
+            untyped __dollar__objset( font, h, untyped __dollar__objget( _f, h ) );
+        }
+
+        for( field in [ 
+            "underline_thickness", "underline_position", 
+            "height", "ascender", "descender", "units_per_EM"
+            ] ) {
+            var h = untyped __dollar__hash(field.__s);
+            untyped __dollar__objset( font, h, untyped __dollar__objget( _f, h ) / _f.units_per_EM );
+        }
+        
+        FT.IterateGlyphs( _f, this );
+        
+    }
+    
+    public function getFont() : Font {
+        return font;
+    }
+    
+    private function _add_glyph( character:Int, g:Glyph ) {
+        if( character > 0 && character < 0xff ) {
+            untyped font.glyphs.__a[character] = g;
+        } else {
+            // non-ascii chars currently ignored
+        }
+    }
+
+    private function endGlyph( character:Int, advance:Int ) {
+        var g:Glyph = new Glyph( polygon, scale*advance );
+        _add_glyph( character, g );
+        polygon = new Polygon();
+        contour = null;
+    }
+
+    private function startContour( x:Int, y:Int ) {
+        contour = new Contour(scale*x,-scale*y);
+    }
+
+    private function endContour() {
+        polygon.addContour( contour );
+        contour = null;
+    }
+
+    private function lineTo( x:Int, y:Int ) {
+        contour.addCoordinates( scale*x, -scale*y );
+    }
+
+    private function curveTo( cx:Int, cy:Int, x:Int, y:Int ) {
+        contour.addQuadratic( new Point(scale*cx,-scale*cy), new Point(scale*x,-scale*y) );
+    }
+}
