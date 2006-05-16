ac0f3c0ba1fade047a07196e5b9a82eba1141d15
dturing <mail@danielturing.com>
2006-05-16 21:24:24 +0000

    more conditional, thus somewhat quirky, handling of "changed" events.

--- org/xinf/value/Value.hx
+++ org/xinf/value/Value.hx
@@ -3,7 +3,19 @@ package org.xinf.value;
 import org.xinf.event.EventDispatcher;
 import org.xinf.event.Event;
 
+/*
+    FIXME: the handling of changed() events is overengineered (uhm, creeping featuritis?)
+*/
+
 class ValueBase extends EventDispatcher {
+    private var _changed:Bool;
+    
+    public function new() :Void {
+        super();
+        _changed = false;
+        addEventListener( "changed", hasChanged );
+    }
+    
     public function get() :Dynamic {
         return null;
     }
@@ -14,10 +26,16 @@ class ValueBase extends EventDispatcher {
     }
 
     private function changed() :Void {
-        this.dispatchEvent( new Event( "changed", this ) );
+        if( hasListeners("changed") && !_changed ) {
+            _changed = true;
+            postEvent( "changed", null );
+        }
+    }
+    private function hasChanged( e:Event ) :Void {
+        _changed = false;
     }
     private function onChildChanged( e:Event ) :Void {
-        this.dispatchEvent( new Event( "changed", this ) );
+        changed();
     }
 
     public function identity() :ValueBase {
@@ -34,8 +52,9 @@ class Value<T> extends ValueBase {
     }
     
     public function set_value( v:T ) :T {
+        var o:T = _value;
         _value=v;
-        changed();
+        if( o != v && o != null ) changed();
         return _value;
     }
     public function get_value() :T {
@@ -48,12 +67,7 @@ class Value<T> extends ValueBase {
     public function set( v:Dynamic ) :Dynamic {
         return set_value( v );
     }
-
-    // FIXME: if refactoring of Style to use Values turns out good, replace all px() bullcrap..
-    public function px() :Float {
-        return cast(value,Float);
-    }        
-    
+   
     public function toString() :String {
         return( ""+get_value() );
     }
@@ -64,11 +78,14 @@ class Value<T> extends ValueBase {
 }
 
 class Identity<T> extends Value<T> {
+    private var listener:Dynamic;
+    
     private var linked:Value<T>;
     public function new( a:Value<T> ) {
         super();
         linked = a;
-        a.addEventListener( "changed", linkChanged );
+        listener = linkChanged;
+        a.addEventListener( "changed", listener );
     }
 
     public function set_value( v:T ) :T {
@@ -85,11 +102,14 @@ class Identity<T> extends Value<T> {
     }
 
     public function setLink( a:Value<T> ) :Void {
-        // FIXME linked.removeEventListener()
+        linked.removeEventListener( "changed", listener );
         var old:T = linked.value;
         linked = a;
-        a.addEventListener( "changed", linkChanged );
-        if( a.value != old ) changed();
+        listener = linkChanged;
+        linked.addEventListener( "changed", listener );
+        if( linked.value != old && old != null ) {
+            changed();
+        }
     }
 
     public function toString() :String {
