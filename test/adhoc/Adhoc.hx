8191545a16e80022c1afbe7143f55131df52c4ec
dturing <mail@danielturing.com>
2006-06-19 10:05:58 +0000

    adhoc test.

---
+++ test/adhoc/Adhoc.hx
@@ -0,0 +1,145 @@
+/* 
+   xinf is not flash.
+   Copyright (c) 2006, Daniel Fischer.
+ 
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+																			
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU		
+   Lesser General Public License or the LICENSE file for more details.
+*/
+
+import org.xinf.event.Event;
+import org.xinf.event.EventDispatcher;
+
+import org.xinf.ul.ListModel;
+import org.xinf.ony.Color;
+
+class Foo extends org.xinf.ony.Text {
+    public function new( name:String, parent:org.xinf.ony.Element ) {
+        super( name, parent );
+        text = "Hello,\nWorld.";
+        autoSize = false;
+        
+        for( event in [ Event.MOUSE_DOWN, Event.MOUSE_UP,
+                        Event.MOUSE_OVER, Event.MOUSE_OUT ] ) {
+            addEventListener( event, handleEvent );
+        }
+    }
+    
+    public function handleEvent( e:Event ) : Void {
+        trace( name+": "+e.type );
+        var t:String = name+"\n"+e.type+"\n";
+        text = t;
+    }
+    
+}
+
+/**
+    Informal xinf Test (used during development).
+**/
+class Adhoc {
+    static var container:org.xinf.ony.Element;
+    
+    static function main() {
+        
+        var root = org.xinf.ony.Root.getRoot();
+
+        var cbg = new org.xinf.ony.Color();
+        cbg.fromRGBInt( 0x003366 );
+
+trace("foo");        
+        var cont = new org.xinf.ony.Pane("container", root);
+        cont.setBackgroundColor( cbg );
+        cont.bounds.setPosition( 10, 10 );
+        cont.bounds.setSize( 300, 200 );
+        container = cont;
+
+trace("bar");
+
+/* listbox test
+        var model = new SimpleListModel();
+        for( i in 0...50 ) {
+            model.addItem( "ListItem "+i );
+        }
+        
+        var list = new org.xinf.ul.ListBox("listTest", cont, model );
+        list.bounds.setSize( 100, 200 );
+*/
+
+/* crop test
+        var a = new Foo( "crop1", cont );
+        a.bounds.setPosition( 120, 25 );
+        a.bounds.setSize( 150, 150 );
+        a.crop = true;
+        a.setBackgroundColor( new Color().fromRGBInt( 0xffaaaa ) );
+
+        var b = new Foo( "tooBig1", a );
+        b.bounds.setPosition( -50, -50 );
+        b.bounds.setSize( 250, 150 );
+        b.setBackgroundColor( new Color().fromRGBInt( 0xaaffaa ) );
+
+        var c = new Foo( "crop2", b );
+        c.crop = true;
+        c.bounds.setPosition( 75, 75 );
+        c.bounds.setSize( 100, 100 );
+        c.setBackgroundColor( new Color().fromRGBInt( 0xaaaaff ) );
+
+        var d = new Foo( "tooBig2", c );
+        d.bounds.setPosition( -25, -25 );
+        d.bounds.setSize( 150, 50 );
+        d.setBackgroundColor( new Color().fromRGBInt( 0xffffaa ) );
+
+        var e = new Foo( "tooBig_fail", a );
+        e.bounds.setPosition( -25, -25 );
+        e.bounds.setSize( 200, 45 );
+        e.setBackgroundColor( new Color().fromRGBInt( 0xff0000 ) );
+*/
+
+/* labels 
+        var bg = new org.xinf.ony.Color();
+        bg.fromRGBInt( 0x336699 );
+        var fg = new org.xinf.ony.Color();
+        fg.fromRGBInt( 0xffffff );
+        
+            for( i in 0...10 ) {
+                var box = new org.xinf.ul.Label("label"+i, cont);
+                box.bounds.setPosition( 10, i*40 );
+                box.text = "Hello "+i;
+               // list.addChild( box );
+            }
+
+        var slider = new org.xinf.ul.VScrollbar( "test", cont );
+*/
+
+
+/* images test 
+        var i = new org.xinf.ony.Image("testImg",cont,"assets/test.jpg");
+        i.bounds.setPosition( 10, 10 );
+        
+        var i3 = new org.xinf.ony.Image("testImg3",cont,"assets/test.png");
+        i3.bounds.setPosition( 25, 25 );
+        */
+        
+/* decorator test, simulate a button 
+
+        var deco = new org.xinf.ul.Decorator("deco",cont);
+        var test = new org.xinf.ul.Label("test button text", deco );
+        deco.setChild( test );
+        test.text = "Hello";
+        deco.bounds.setPosition(120,20);
+*/
+        var test:Dynamic;
+        test = function (e:Event) {
+            trace("frame 1");
+            EventDispatcher.removeGlobalEventListener( Event.ENTER_FRAME, test );
+        };
+        EventDispatcher.addGlobalEventListener( Event.ENTER_FRAME, test );
+
+        org.xinf.ony.Root.getRoot().run();
+    }
+}
