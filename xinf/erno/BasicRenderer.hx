7ef69e3920c165f0711b8e3f2c4a4be21c44fc58
dturing <mail@danielturing.com>
2006-12-12 01:11:24 +0000

    forgot new BasicRenderer: implements all Renderer functions, throwing "unimplemented"

---
+++ xinf/erno/BasicRenderer.hx
@@ -0,0 +1,112 @@
+/* 
+   xinf is not flash.
+   Copyright (c) 2006, Daniel Fischer.
+ 
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+																			
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU		
+   Lesser General Public License or the LICENSE file for more details.
+*/
+
+package xinf.erno;
+
+import xinf.erno.Renderer;
+
+class BasicRenderer implements Renderer {
+	public function new() :Void {
+	}
+
+	public function getNextId() :Int {
+		throw("unimplemented");
+		return -1;
+	}
+	public function getRootId() :Int {
+		throw("unimplemented");
+		return -1;
+	}
+
+	// erno Instruction protocol
+	
+	public function startObject( id:Int ) {
+		throw("unimplemented");
+	}
+	public function endObject() {
+		throw("unimplemented");
+	}
+	public function showObject( id:Int ) {
+		throw("unimplemented");
+	}
+
+	public function translate( x:Float, y:Float ) {
+		throw("unimplemented");
+	}
+	public function scale( x:Float, y:Float ) {
+		throw("unimplemented");
+	}
+	public function rotate( angle:Float ) {
+		throw("unimplemented");
+	}
+	public function transform( matrix:Matrix ) {
+		throw("unimplemented");
+	}
+	public function clipRect( w:Float, h:Float ) {
+		throw("unimplemented");
+	}
+
+	public function setFill( c:Color ) {
+		throw("unimplemented");
+	}
+	public function setStroke( c:Color, width:Float ) {
+		throw("unimplemented");
+	}
+	public function setFont( face:String, slant:FontSlant, weight:FontWeight, size:Float ) {
+		throw("unimplemented");
+	}
+
+	public function startShape() {
+		throw("unimplemented");
+	}
+	public function endShape() {
+		throw("unimplemented");
+	}
+	public function startPath( x:Float, y:Float) {
+		throw("unimplemented");
+	}
+	public function endPath() {
+		throw("unimplemented");
+	}
+	public function close() {
+		throw("unimplemented");
+	}
+	public function lineTo( x:Float, y:Float ) {
+		throw("unimplemented");
+	}
+	public function quadraticTo( x1:Float, y1:Float, x:Float, y:Float ) {
+		throw("unimplemented");
+	}
+	public function cubicTo( x1:Float, y1:Float, x2:Float, y2:Float, x:Float, y:Float ) {
+		throw("unimplemented");
+	}
+
+	public function rect( x:Float, y:Float, w:Float, h:Float ) {
+		throw("unimplemented");
+	}
+	public function circle( x:Float, y:Float, r:Float ) {
+		throw("unimplemented");
+	}
+	public function text( x:Float, y:Float, text:String, ?style:FontStyle ) {
+		throw("unimplemented");
+	}
+	public function image( img:ImageData, inRegion:{ x:Float, y:Float, w:Float, h:Float }, outRegion:{ x:Float, y:Float, w:Float, h:Float } ) {
+		throw("unimplemented");
+	}
+	
+	public function native( o:Dynamic ) {
+		throw("unimplemented");
+	}
+}
