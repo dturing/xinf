12863f84f4cdb33d16b8b27201e3dbd75bc441ef
dturing <mail@danielturing.com>
2006-12-16 02:53:34 +0000

    up opengl version

--- xinf/Version.hx
+++ xinf/Version.hx
@@ -2,7 +2,7 @@ package xinf;
 
 class Version {
 	public static var version:String = "0.0.0";
-	public static var revision:String = "506:511M";
-	public static var built:String = "2006-12-16 03:25:26";
+	public static var revision:String = "506:512M";
+	public static var built:String = "2006-12-16 03:51:17";
 	public static var tagline:String = "";
 }
