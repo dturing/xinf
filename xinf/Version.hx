d124adabe86ee8b5864e1a2caa367d1a80feaa22
dturing <mail@danielturing.com>
2007-06-21 15:29:23 +0000

    flash trace

--- xinf/Version.hx
+++ xinf/Version.hx
@@ -2,7 +2,7 @@ package xinf;
 
 class Version {
 	public static var version:String = "0.0.6";
-	public static var revision:String = "605:692M";
-	public static var built:String = "2007-06-06 18:25:20";
+	public static var revision:String = "605:705M";
+	public static var built:String = "2007-06-14 16:32:53";
 	public static var tagline:String = "";
 }
