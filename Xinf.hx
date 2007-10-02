05b9427ebe08f5258975a0ddf8262ca7aba996ac
dturing <mail@danielturing.com>
2007-10-02 17:41:01 +0000

    add missing master include file Xinf.hx

---
+++ Xinf.hx
@@ -0,0 +1,24 @@
+
+import xinf.geom.Types;
+
+typedef Color = xinf.erno.Color
+typedef Translate = xinf.geom.Translate
+typedef TPoint = xinf.geom.TPoint
+
+typedef MouseEvent = xinf.event.MouseEvent
+typedef KeyboardEvent = xinf.event.KeyboardEvent
+typedef FrameEvent = xinf.event.FrameEvent
+
+typedef Root = xinf.ony.Root
+typedef Element = xinf.ony.erno.Object
+typedef Document = xinf.ony.erno.Document
+typedef Group = xinf.ony.erno.Group
+typedef Rectangle = xinf.ony.erno.Rectangle
+typedef Ellipse = xinf.ony.erno.Ellipse
+typedef Circle = xinf.ony.erno.Circle
+typedef Image = xinf.ony.erno.Image
+typedef Path = xinf.ony.erno.Path
+typedef Line = xinf.ony.erno.Line
+typedef Polygon = xinf.ony.erno.Polygon
+typedef Text = xinf.ony.erno.Text
+
