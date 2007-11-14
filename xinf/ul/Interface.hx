a93c791c124070bb904d4f33ea6c8fcb12471602
dturing <mail@danielturing.com>
2007-11-14 21:17:01 +0000

    small fix

--- xinf/ul/Interface.hx
+++ xinf/ul/Interface.hx
@@ -25,7 +25,7 @@ class Interface extends Container {
 	public function new( g:Group ) {
 		super( g );
 
-		var a = g.getTypedChildByName( "area", xinf.ony.Rectangle );
+		var a = g.getTypedElementByName( "area", xinf.ony.Rectangle );
 		area = { l:a.x, t:a.y, r:a.x+a.width, b:a.y+a.height };
 		setPrefSize( { x:area.r-area.l, y:area.b-area.t } );
 	}
