65280ecbe154d9279aa764f130be39df4029768a
dturing <mail@danielturing.com>
2007-12-02 18:57:02 +0000

    drag demo

--- samples/drag/Demo.hx
+++ samples/drag/Demo.hx
@@ -1,19 +1,19 @@
 import Xinf;
 
-class Demo {
-	
-	var r:Rectangle;
+class DraggableRectangle extends Rectangle {
 	
 	public function new() :Void {
-	
-		r = new Rectangle();
-		r.style.fill = Color.RED;
-		r.x=r.y=100;
-		r.width=r.height=100;
-	
-		r.addEventListener( MouseEvent.MOUSE_DOWN, onMouseDown );
+		super();
 		
-		Root.attach( r );
+		style.fill = Color.BLUE;
+		x=y=100;
+		width=height=100;
+		addEventListener( MouseEvent.MOUSE_DOWN, onMouseDown );
+		
+		var self=this;
+		addEventListener( MouseEvent.MOUSE_OVER, function(e) { self.style.fill = Color.RED; } );
+		addEventListener( MouseEvent.MOUSE_OUT, function(e) { self.style.fill = Color.BLUE; } );
+
 	}
 	
 	public function onMouseDown( e:MouseEvent ) :Void {
@@ -22,7 +22,10 @@ class Demo {
 		var moveL:Dynamic;
 		var self = this;
 		
+		style.fill = Color.GREEN;
+		
 		upL = Root.addEventListener( MouseEvent.MOUSE_UP, function(e) {
+				self.style.fill = Color.RED;
 				Root.removeEventListener( MouseEvent.MOUSE_UP, upL );
 				Root.removeEventListener( MouseEvent.MOUSE_MOVE, moveL );
 			} );
@@ -30,9 +33,17 @@ class Demo {
 		moveL = Root.addEventListener( MouseEvent.MOUSE_MOVE, function(e) {
 				var o = { x:ofs.x-e.x, y:ofs.y-e.y };
 				ofs = e;
-				self.r.x -= o.x;
-				self.r.y -= o.y;
+				self.x -= o.x;
+				self.y -= o.y;
 			} );
+	}
+	
+}
+
+class Demo {
+	
+	public function new() :Void {
+		Root.attach( new DraggableRectangle() );
 	}
 	
 	public static function main() :Void {
