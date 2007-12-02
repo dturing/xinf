7510a0a3702b9fc1743524480e303e22e2420ad6
dturing <mail@danielturing.com>
2007-12-02 20:38:10 +0000

    add xml binding modifiers to erno.Document

--- xinf/ony/erno/Document.hx
+++ xinf/ony/erno/Document.hx
@@ -4,6 +4,7 @@ import xinf.erno.Renderer;
 import xinf.erno.Runtime;
 import xinf.ony.URL;
 import xinf.xml.Binding;
+import xinf.xml.Instantiator;
 
 class Document extends xinf.ony.base.Document {
 
@@ -29,7 +30,15 @@ class Document extends xinf.ony.base.Document {
     public static function load( url_s:String, ?onLoad:Document->Void ) :Document {
 		return xinf.ony.base.Document.load( url_s, onLoad );
 	}
-	
+
+    public static function overrideBinding( nodeName:String, cl:Class<Element> ) :Void {
+		return xinf.ony.base.Document.overrideBinding( nodeName, cl );
+    }
+    
+    public static function addInstantiator( i:Instantiator<Element> ) :Void {
+		return xinf.ony.base.Document.addInstantiator( i );
+    }
+
 	static function __init__() :Void {
         var binding = new Binding<Element>();
         
