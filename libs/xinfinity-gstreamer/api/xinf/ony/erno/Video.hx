d44588945799852e7429fc9534b01eee94f3599d
dturing <mail@danielturing.com>
2008-01-23 18:25:29 +0000

    import the naive Video element from media branch.

---
+++ libs/xinfinity-gstreamer/api/xinf/ony/erno/Video.hx
@@ -0,0 +1,66 @@
+/*  Copyright (c) the Xinf contributors.
+    see http://xinf.org/copyright for license. */
+	
+package xinf.ony.erno;
+
+import xinf.xml.URL;
+import xinf.erno.Renderer;
+import xinf.inity.gst.VideoSource;
+import xinf.event.ImageLoadEvent;
+import xinf.erno.Paint;
+import xinf.ony.traits.PreserveAspectRatioTrait;
+
+class Video extends xinf.ony.Video {
+
+	var source:VideoSource;
+	var srcElement:gst.Object;
+	
+	var pipeTest:xinf.inity.gst.Pipeline;
+	
+	public function new( ?traits:Dynamic ) :Void {
+		super(traits);
+	}
+
+    override function set_href( v:String ) :String { 
+		setTrait("href",v);
+		applyHref();
+        return href;
+	}
+
+	override public function onLoad() :Void {
+		super.onLoad();
+		applyHref();
+	}
+	
+	function applyHref() {
+		trace("video: apply href "+href );
+		//source.pipeline.pause();
+		//srcElement.set("location", href );
+		//source.pipeline.play();
+		
+		//source = new VideoSource("videotestsrc",
+		source = new VideoSource("gnomevfssrc name=src location=\""+href+"\" ! decodebin name=d ! audioconvert ! alsasink  d. ! videoscale",
+			320, 240 );
+		srcElement = source.pipeline.findChild("src");
+		//pipeTest = new xinf.inity.gst.Pipeline("fakesrc ! video/x-raw-yuv, framerate=(fraction)1 ! queue ! identity ! queue ! identity ! queue ! identity ! queue ! identity ! queue ! fakesink sync=true");
+    }
+	
+	override public function drawContents( g:Renderer ) :Void {
+
+		if( source==null ) return;
+
+		if( width<=0 ) width = source.width;
+		if( height<=0 ) height = source.height;
+		
+		var box = PreserveAspectRatioTrait.align( preserveAspectRatio,
+			{ x:source.width, y:source.height }, { x:width, y:height } );
+
+		g.setFill( SolidColor(1,1,1,opacity) );
+		box.x+=x; box.y+=y;
+		
+		if( opacity > 0 || opacity==null ) {
+			g.image( source, {x:0.,y:0.,w:source.width,h:source.height}, box );
+		}
+     }
+    
+}
