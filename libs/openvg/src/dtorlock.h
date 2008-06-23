57f20f8d26d5e59afc4de960004ae229f0188a05
dturing <mail@danielturing.com>
2008-06-23 12:21:57 +0000

    add neko's context.h/.c, and add -fno-common for OSX; these are temporary until the context lock functionality is exported from neko.

--- libs/openvg/src/dtorlock.h
+++ libs/openvg/src/dtorlock.h
@@ -1,12 +1,7 @@
 #ifndef _DTOR_LOCK
 #define _DTOR_LOCK
 
-typedef struct _clock _clock;
-
-_clock *context_lock_new();
-void context_lock( _clock *l );
-void context_release( _clock *l );
-void context_lock_delete( _clock *l );
+#include "context.h"
 
 extern _clock *VGPath_dtorLock;
 extern _clock *VGPaint_dtorLock;
