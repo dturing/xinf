9da75c890e821a2ee44975a1d34336e13eb384ac
dturing <mail@danielturing.com>
2008-08-24 18:33:45 +0000

    GLRenderer renders text stroke as "shadow" (+1/1 px) below filled text.

--- xinf/inity/GLRenderer.hx
+++ xinf/inity/GLRenderer.hx
@@ -182,15 +182,25 @@ class GLRenderer extends ObjectModelRenderer {
 		format.assureLoaded();
 		var font = format.font;
 		if( font==null ) trace("NULL font");
-		if( pen.fill != null && font != null ) {
-			GL.pushMatrix();
-				GL.translate( Math.floor(x), Math.floor(y), 0 );
-				applyFillGL();
-				GL.enable(GL.BLEND);
-				font.renderText( text, format.size );
-				GL.disable(GL.BLEND);
-				
-			GL.popMatrix();
+		if( font != null ) {
+			if( pen.stroke != null ) {
+				GL.pushMatrix();
+					GL.translate( Math.floor(x+1), Math.floor(y+1), 0 );
+					applyStrokeGL();
+					GL.enable(GL.BLEND);
+					font.renderText( text, format.size );
+					GL.disable(GL.BLEND);
+				GL.popMatrix();
+			}
+			if( pen.fill != null ) {
+				GL.pushMatrix();
+					GL.translate( Math.floor(x), Math.floor(y), 0 );
+					applyFillGL();
+					GL.enable(GL.BLEND);
+					font.renderText( text, format.size );
+					GL.disable(GL.BLEND);
+				GL.popMatrix();
+			}
 		}
 		// TODO: addHitRectangle
 	}
