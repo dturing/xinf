8263d67eb67b27bbef23a2d019085026e78b3c96
dturing <mail@danielturing.com>
2008-12-21 15:14:37 +0000

    js: use "px" unit (should fix "strict mode")

--- xinf/erno/js/JSRenderer.hx
+++ xinf/erno/js/JSRenderer.hx
@@ -34,26 +34,26 @@ class JSRenderer extends ObjectModelRenderer {
 	
 	override public function setPrimitiveTransform( p:Primitive, x:Float, y:Float, a:Float, b:Float, c:Float, d:Float ) :Void {
 		// FIXME (maybe): regards only translation
-		p.style.left = ""+Math.round(x);
-		p.style.top = ""+Math.round(y);
+		p.style.left = ""+Math.round(x)+"px";
+		p.style.top = ""+Math.round(y)+"px";
 	}
 
 	override public function setPrimitiveTranslation( p:Primitive, x:Float, y:Float ) :Void {
-		p.style.left = ""+Math.round(x);
-		p.style.top = ""+Math.round(y);
+		p.style.left = ""+Math.round(x)+"px";
+		p.style.top = ""+Math.round(y)+"px";
 	}
 
 	override public function clipRect( w:Float, h:Float ) {
 		current.style.overflow = "hidden";
-		current.style.width = ""+Math.max(0,Math.round(w));
-		current.style.height = ""+Math.max(0,Math.round(h));
+		current.style.width = ""+Math.max(0,Math.round(w))+"px";
+		current.style.height = ""+Math.max(0,Math.round(h))+"px";
 	}
 	
 	override public function rect( x:Float, y:Float, w:Float, h:Float ) {
 		var r = js.Lib.document.createElement("div");
 		r.style.position="absolute";
-		r.style.left = ""+Math.round(x);
-		r.style.top = ""+Math.round(y);
+		r.style.left = ""+Math.round(x)+"px";
+		r.style.top = ""+Math.round(y)+"px";
 		if( pen.fill != null ) {
 			switch( pen.fill ) {
 				case SolidColor(red,g,b,a):
@@ -68,15 +68,15 @@ class JSRenderer extends ObjectModelRenderer {
 				case SolidColor(red,g,b,a):
 					// FIXME: a
 					r.style.border = ""+pen.width+"px solid "+colorToRGBString(red,g,b);
-					r.style.width = ""+Math.round(w+1-(pen.width*2));
-					r.style.height = ""+Math.round(h+1-(pen.width*2));
+					r.style.width = ""+Math.round(w+1-(pen.width*2))+"px";
+					r.style.height = ""+Math.round(h+1-(pen.width*2))+"px";
 				default:
 					r.style.border = 0;
 			}
 		}
 		
-		r.style.width = ""+Math.round(w);
-		r.style.height = ""+Math.round(h);
+		r.style.width = ""+Math.round(w)+"px";
+		r.style.height = ""+Math.round(h)+"px";
 		current.appendChild( r );
 	}
 
@@ -93,8 +93,8 @@ class JSRenderer extends ObjectModelRenderer {
 		r.style.position="absolute";
 		r.style.whiteSpace="nowrap";
 		r.style.cursor="default";
-		if( x!=null ) r.style.left = ""+Math.round(x);
-		if( y!=null ) r.style.top = ""+Math.round(y);
+		if( x!=null ) r.style.left = ""+Math.round(x)+"px";
+		if( y!=null ) r.style.top = ""+Math.round(y)+"px";
 		
 		if( pen.fill != null ) {
 			switch( pen.fill ) {
@@ -123,18 +123,18 @@ class JSRenderer extends ObjectModelRenderer {
 		var r:Image = cast(js.Lib.document.createElement("img"));
 		r.src = img.url;
 		r.style.position = "absolute";
-		r.style.left = ""+Math.round(-inRegion.x*wf);
-		r.style.top = ""+Math.round(-inRegion.y*hf);
-		r.style.width = ""+Math.round( img.width * wf );
-		r.style.height = ""+Math.round( img.height * hf );
+		r.style.left = ""+Math.round(-inRegion.x*wf)+"px";
+		r.style.top = ""+Math.round(-inRegion.y*hf)+"px";
+		r.style.width = ""+Math.round( img.width * wf )+"px";
+		r.style.height = ""+Math.round( img.height * hf )+"px";
 		
 		var wrap = js.Lib.document.createElement("div");
 		wrap.style.position = "absolute";
 		wrap.style.overflow = "hidden";
-		wrap.style.left = ""+Math.round(outRegion.x);
-		wrap.style.top = ""+Math.round(outRegion.y);
-		wrap.style.width = ""+Math.round(outRegion.w);
-		wrap.style.height = ""+Math.round(outRegion.h);
+		wrap.style.left = ""+Math.round(outRegion.x)+"px";
+		wrap.style.top = ""+Math.round(outRegion.y)+"px";
+		wrap.style.width = ""+Math.round(outRegion.w)+"px";
+		wrap.style.height = ""+Math.round(outRegion.h)+"px";
 		
 		wrap.appendChild(r);
 		
